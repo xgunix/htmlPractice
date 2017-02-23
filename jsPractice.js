334955145ca069c7b6ce4958b054dac09e0bba3b
bhsj_imac <bhsj_imac@bhsj-imacdeiMac.local>
2017-02-23 16:42:58 +0800

    js练习

---
+++ jsPractice.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    javaScript能直接写入HTML输出流中。
+    </p>
+    <script>
+        document.write("<h1>THis is a heading</h1>");
+        document.write("<p>This is a paragraph</p>");
+    </script>
+</body>
+</html>
